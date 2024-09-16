867a0710ad445f788180178cc542756a63760d0f
IllTamer <mail@illtamer.com>
2024-09-17 00:35:54 +0800

    feat: add 2fa auth

--- cmd/rhttp/api_v1_auth.go
+++ cmd/rhttp/api_v1_auth.go
@@ -1,6 +1,8 @@
 package rhttp
 
 import (
+	"github.com/IUnlimit/ssh2a/conf"
+	"github.com/IUnlimit/ssh2a/tools"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"net/http"
@@ -27,10 +29,10 @@ func auth(c *gin.Context) {
 }
 
 func authorization(password string, remoteIP string, c *gin.Context) {
-	if password != "666" {
-		log.Warnf("Worng password(%s) tried by ip(%s)", password, remoteIP)
+	if !checkAccess(password) {
+		log.Warnf("Worng password or 2fa key(%s) tried by ip(%s)", password, remoteIP)
 		c.JSON(http.StatusForbidden, gin.H{
-			"message": "Wrong password",
+			"message": "Wrong password or 2fa key",
 		})
 		err := IPCache.UpdateIPStatus(remoteIP, false)
 		if err != nil {
@@ -47,4 +49,19 @@ func authorization(password string, remoteIP string, c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{})
 	return
+}
+
+func checkAccess(password string) bool {
+	config := conf.Config.Authorization
+	if config.Type == "basic" {
+		return password == config.Basic.Secret
+	} else if config.Type == "authenticator" {
+		totP, err := tools.TotP(config.Authenticator.PrivateSecret)
+		if err != nil {
+			log.Errorf("Failed to get 2fa key, %v", err)
+			return false
+		}
+		return password == totP
+	}
+	return false
 }
